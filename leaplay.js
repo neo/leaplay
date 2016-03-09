c9b215d290d1d6455b529d1487f3269e7d88926c
Neo <neolwc@gmail.com>
2016-03-09 16:48:25 -0500

    Add grab & create

--- leaplay.js
+++ leaplay.js
@@ -13,10 +13,13 @@ camera.lookAt(new THREE.Vector3(0,250,0));
 var material = new THREE.MeshNormalMaterial();
 
 var hands = new THREE.Object3D();
-scene.add(hands);
+var cubes = new THREE.Object3D();
+scene.add(hands, cubes);
 
 var ballGeometry = new THREE.SphereGeometry(10,32,32);
 for (var i = 0; i < 5; i++) hands.add(new THREE.Mesh(ballGeometry, material));
+
+var cubeGeometry = new THREE.BoxGeometry(50,50,50);
 
 var render = function () {
 	requestAnimationFrame( render );
@@ -24,6 +27,9 @@ var render = function () {
 };
 render();
 
+var isGrab = false;
+var soSmall = 0.01;
+var currentCube;
 Leap.loop(function (frame) {
 	if (frame.hands.length) {
 		var hand = frame.hands[0];
@@ -31,5 +37,18 @@ Leap.loop(function (frame) {
 			var dip = hand.fingers[i].dipPosition;
 			hands.children[i].position.fromArray(dip);
 		}
+		// console.log(hand.grabStrength, hand.pinchStrength);
+		if (hand.grabStrength === 1 && !isGrab) {
+			isGrab = true;
+			cubes.add(new THREE.Mesh(cubeGeometry, material));
+			currentCube = cubes.children[cubes.children.length - 1];
+			currentCube.position.fromArray(hand.palmPosition);
+			currentCube.scale.set(soSmall,soSmall,soSmall);
+		}
+		if (hand.grabStrength < 1 && isGrab) {
+			var scale = (1 - hand.grabStrength) ? 1 - hand.grabStrength : soSmall;
+			currentCube.scale.set(scale,scale,scale);
+		}
+		if (hand.grabStrength === 0 && isGrab) isGrab = false;
 	}
 });
